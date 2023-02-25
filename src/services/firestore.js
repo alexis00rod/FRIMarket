905e4ab9158735df779ff1e45180711cb7890043
alexis00rod <alexis00rodrigo@gmail.com>
2023-02-25 15:41:58 -0300

    Agregue funcion para obtener productos

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,7 +1,10 @@
 import { app } from "./firebase";
-import { collection, getDocs, getFirestore } from 'firebase/firestore'
+import { collection, getDocs, getFirestore, orderBy, query, where } from 'firebase/firestore'
 
 const db = getFirestore(app)
+
+// Referencias
+const productsRef = collection(db,"products")
 
 // Funcion para obtener categorias
 export const getCategories = async () => {
@@ -9,6 +12,16 @@ export const getCategories = async () => {
 }
 
 // Funcion para obtener productos
-export const getProducts = async () => {
-  return await getDocs(collection(db, "products"))
+export const getProducts = async (category,filters) => {
+  const {minPrice, maxPrice,sort} = filters
+
+  const order = sort === 'lowPrice'
+    ? orderBy('price','asc')
+    : orderBy('price','desc')
+
+  const q = category === 'all'
+    ? query(productsRef,where('price','>=',minPrice),where('price','<=',maxPrice),order)
+    : query(productsRef,where('category','==',category),where('price','>=',minPrice),where('price','<=',maxPrice),order)
+
+  return await getDocs(q)
 }
