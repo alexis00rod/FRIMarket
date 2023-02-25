d8819a2bb271c879ba6d2806152332ac531ed3bc
alexis00rod <alexis00rodrigo@gmail.com>
2023-02-25 01:45:46 -0300

    Instale firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOBl99P_cWnls3Vgu4SJmjt526DS7Ishw",
+  authDomain: "frimarket-f4864.firebaseapp.com",
+  projectId: "frimarket-f4864",
+  storageBucket: "frimarket-f4864.appspot.com",
+  messagingSenderId: "658721017033",
+  appId: "1:658721017033:web:ee2b1331a7e8d3fe2ed118"
+};
+
+export const app = initializeApp(firebaseConfig);
