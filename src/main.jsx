e2f70f58f9542336cdcfd623a73be6ff656879d8
alexis00rod <alexis00rodrigo@gmail.com>
2023-02-27 17:09:09 -0300

    Configure cart conext provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './components'
+import { CartContextProvider } from './context/CartContext/CartContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
