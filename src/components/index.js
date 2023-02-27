ad52d0d56ead3a2f46bafd7c61469322a90b9b84
alexis00rod <alexis00rodrigo@gmail.com>
2023-02-27 08:55:17 -0300

    Configure componentes loader, product modal y product layout

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,10 @@
 import { Accordion } from "./Accordion/Accordion";
 import { App } from "./App/App";
 import { Footer } from "./Footer/Footer";
+import { Loader } from "./Loader/Loader";
 import { Navbar } from "./Navbar/Navbar";
 import { ProductCard } from "./ProductCard/ProductCard";
+import { ProductModal } from "./ProductModal/ProductModal";
+import { ProductsLayout } from "./ProductsLayout/ProductsLayout";
 
-export {App,Navbar,Footer,Accordion,ProductCard}
+export {App,Navbar,Footer,Accordion,ProductCard,ProductModal,ProductsLayout,Loader}
