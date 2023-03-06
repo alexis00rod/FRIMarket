ca6e7f3ce107ce66342e1544d26bd1df906dba99
alexis00rod <alexis00rodrigo@gmail.com>
2023-03-06 16:19:29 -0300

    Agregue auth context

--- src/context/AuthContext/AuthContext.jsx
+++ src/context/AuthContext/AuthContext.jsx
@@ -1,13 +1,18 @@
 import { useState, useEffect, createContext, useContext } from 'react'
+import { stateAuth } from '../../services/auth'
 
 const AuthContext = createContext()
 export const useAuthContext = () => useContext(AuthContext)
 
 export const AuthContextProvider = ({children}) => {
-  const [userLogged, setUserLogged] = useState('rodalrd')
+  const [userLogged, setUserLogged] = useState()
+
+  useEffect(() => {
+    stateAuth(setUserLogged)
+  },[])
 
   return (
-    <AuthContext.Provider value={userLogged}>
+    <AuthContext.Provider value={{userLogged}}>
       {children}
     </AuthContext.Provider>
   )
