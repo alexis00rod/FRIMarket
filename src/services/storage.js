a8148d5ba8de029976556d4d4c44ddcd42f0a11e
alexis00rod <alexis00rodrigo@gmail.com>
2023-03-07 15:58:13 -0300

    Cree funcion para subir archivo

--- src/services/storage.js
+++ src/services/storage.js
@@ -4,10 +4,18 @@ const storage = getStorage()
 
 // Referencias
 const productThumbRef = (name) => ref(storage, `products/${name}`)
+const userPhotoRef = (name) => ref(storage, `users/${name}`)
 
 
 export const uploadThumb = async (file) => {
   await uploadBytes(productThumbRef(file.name),file)
   const url = await getDownloadURL(productThumbRef(file.name))
+  return url
+}
+
+export const uploadToStorage = async (file) => {
+  const {name} = file
+  await uploadBytes(userPhotoRef(name),file)
+  const url = await getDownloadURL(userPhotoRef(name))
   return url
 }
