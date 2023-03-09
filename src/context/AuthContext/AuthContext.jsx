49f35a51b09115ecc0bfc9957ec50c1fdb03e0e7
alexis00rod <alexis00rodrigo@gmail.com>
2023-03-09 15:59:09 -0300

    Agregue funcion para obtener perfil del usuario logeado

--- src/context/AuthContext/AuthContext.jsx
+++ src/context/AuthContext/AuthContext.jsx
@@ -1,18 +1,25 @@
 import { useState, useEffect, createContext, useContext } from 'react'
 import { stateAuth } from '../../services/auth'
+import { getUser } from '../../services/firestore'
 
 const AuthContext = createContext()
 export const useAuthContext = () => useContext(AuthContext)
 
 export const AuthContextProvider = ({children}) => {
   const [userLogged, setUserLogged] = useState()
+  const [userLoggedProfile, setUserLoggedProfile] = useState({})
 
   useEffect(() => {
     stateAuth(setUserLogged)
   },[])
+  
+  useEffect(() => {
+    userLogged &&
+    getUser(userLogged,setUserLoggedProfile)
+  },[userLogged])
 
   return (
-    <AuthContext.Provider value={{userLogged}}>
+    <AuthContext.Provider value={{userLogged, userLoggedProfile}}>
       {children}
     </AuthContext.Provider>
   )
