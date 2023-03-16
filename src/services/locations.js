63c93ccdd9923753c402d2f533124ff891048d50
alexis00rod <alexis00rodrigo@gmail.com>
2023-03-16 09:00:01 -0300

    Agregue funcion para obtener ubicacion

--- src/services/locations.js
+++ src/services/locations.js
@@ -5,3 +5,10 @@ export const getLocations = async  () => {
   const {provincias} = json
   return provincias
 }
+
+export const getLocation = async (id) => {
+  const request = await fetch(`https://apis.datos.gob.ar/georef/api/provincias?id=${id}`)
+  const json = await request.json()
+  const {provincias} = json
+  return provincias[0]
+}
