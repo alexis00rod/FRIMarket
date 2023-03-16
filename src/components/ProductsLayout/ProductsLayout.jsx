a75e482fc3cde9d9b6314b3bd1f44d652cc0488a
alexis00rod <alexis00rodrigo@gmail.com>
2023-03-16 11:19:26 -0300

    Agregue funcion para ordenar productos

--- src/components/ProductsLayout/ProductsLayout.jsx
+++ src/components/ProductsLayout/ProductsLayout.jsx
@@ -1,12 +1,30 @@
 import { Loader, ProductCard } from "../index.js"
 
-export const ProductsLayout = ({products, layout}) => {
+export const ProductsLayout = ({products, layout, sort}) => {
+
+  const order = (a, b) => {
+    if(sort === 'highPrice') {
+      if(a.price < b.price) return 1
+      if(a.price > b.price) return -1
+      return 0
+    }
+    if(sort === 'lowPrice') {
+      if(a.price > b.price) return 1
+      if(a.price < b.price) return -1
+      return 0
+    }
+    if(sort === 'postDate') {
+      if(a.timestamp < b.timestamp) return 1
+      if(a.timestamp > b.timestamp) return -1
+      return 0
+    }
+  }
 
   if(!products) return <Loader />
 
   return (
     <div className={`w-full grid ${layout === 'grid' ? 'grid-cols-5' : 'grid-cols-1'} gap-4`}>
-      {products.map(element => <ProductCard key={element.id} content={element} style={layout} />)}
+      {products.sort(order).map(element => <ProductCard key={element.id} content={element} style={layout} />)}
     </div>
   )
 }
