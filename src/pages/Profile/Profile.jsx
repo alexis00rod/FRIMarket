b454ec0a72b1edd7e52f786eecfc738f2db0f4f8
alexis00rod <alexis00rodrigo@gmail.com>
2023-03-17 13:03:53 -0300

    Corregui componente ProductLayout y agregue ProductsList

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { Loader, ProfileInfo } from '../../components/index.js'
+import { Loader, ProductsLayout, ProductsSort, ProfileInfo } from '../../components/index.js'
 import { ProfilePosts } from "../../components/index.js"
+import { useCardSize } from "../../hooks/useCardSize.jsx"
+import { useProductsSort } from "../../hooks/useProductsSort.jsx"
 import { getUserById } from "../../services/firestore"
 
 export const Profile = () => {
   const {idUser} = useParams()
   const [userProfile, setUserProfile] = useState()
+  const {productsSort, setProductsSort} = useProductsSort()
+  const {cardSize, setCardSize} = useCardSize()
 
   useEffect(() => {
     getUserById(idUser, setUserProfile)
@@ -19,8 +23,13 @@ export const Profile = () => {
       <aside className="w-full max-w-xs h-max px-2 py-2 flex flex-col flex-none bg-white border border-gray-300 divide-y divide-gray-300 rounded-md">
         <ProfileInfo user={userProfile} />
       </aside>
-      <section className="w-full px-2">
-        <ProfilePosts user={userProfile} />
+      <section className="w-full px-2 flex flex-col gap-4">
+        <div className="w-full px-2 py-2 flex items-center bg-white border border-gray-300 rounded-md">
+          <h3 className="px-2 py-2 grow text-lg font-medium">Publicaciones</h3>
+          <ProductsSort selected={productsSort} onChange={({target: {id}}) => setProductsSort(id)} />
+          <ProductsLayout size={cardSize}  onChange={({target:{id}}) => setCardSize(id)} />
+        </div>
+        <ProfilePosts user={userProfile} sort={productsSort} layout={cardSize} />
       </section>
     </div>
   )
--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react"
-import { ProductsLayout } from "../../components"
+import { ProductsLayout, ProductsList, ProductsSort } from "../../components"
 import { useAuthContext } from "../../context/AuthContext/AuthContext"
+import { useCardSize } from "../../hooks/useCardSize"
+import { useProductsSort } from "../../hooks/useProductsSort"
 import { getWishlist } from "../../services/firestore"
 
 export const Wishlist = () => {
   const {userLogged} = useAuthContext()
   const [wishlist, setWishlist] = useState([])
+  const {cardSize, setCardSize} = useCardSize()
+  const {productsSort, setProductsSort} = useProductsSort()
 
   useEffect(() => {
     getWishlist(userLogged)
@@ -17,11 +21,13 @@ export const Wishlist = () => {
 
   return (
     <div className="w-full flex flex-col gap-4 grow">
-      <div className="w-full px-2 py-2 bg-white border border-gray-300 rounded-md">
-        <h2 className="px-3 py-3 text-xl font-semibold">Lista de favoritos</h2>
+      <div className="w-full px-2 py-2 flex items-center bg-white border border-gray-300 rounded-md">
+        <h2 className="px-3 py-3 grow text-xl font-semibold">Lista de favoritos</h2>
+        <ProductsSort selected={productsSort} onChange={({target: {id}}) => setProductsSort(id)} />
+        <ProductsLayout size={cardSize}  onChange={({target:{id}}) => setCardSize(id)} />
       </div>
       <section className="w-full">
-        <ProductsLayout products={wishlist} layout='grid' sort='postDate' />
+        <ProductsList products={wishlist} sort={productsSort} size={cardSize} maxCols={5} />
       </section>
     </div>
   )
