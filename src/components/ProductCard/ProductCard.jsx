1a9c742414ea019576d6a924f4f4f59f7ca75cb5
alexis00rod <alexis00rodrigo@gmail.com>
2023-03-17 12:03:23 -0300

    Agregue estilos para card size s y l

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -6,7 +6,7 @@ import { BtnAddCart, BtnAddWishlist, ProductModal } from '../index.js'
 
 const Card = ({size,direction,children}) => {
   return (
-    <article className={`${size} px-2 py-2 flex flex-${direction} flex-none gap-2 bg-white border border-gray-300 rounded-md`}>
+    <article className={`${size} px-2 py-2 flex flex-${direction} flex-none bg-white border border-gray-300 rounded-md`}>
       {children}
     </article>
   )
@@ -14,23 +14,23 @@ const Card = ({size,direction,children}) => {
 
 const Thumb = ({id,size,thumb,name}) => {
   return (
-    <Link to={`/product/${id}`} className={`${size} px-1 py-1 flex items-center justify-center`}>
-      <img src={thumb} alt={name} className='w-full h-full object-contain' />
+    <Link to={`/product/${id}`} className={`${size} px-1 py-1 flex items-center justify-center flex-none overflow-hidden`}>
+      <img src={thumb} alt={name} className='h-full object-contain' />
     </Link>
   )
 }
 
-const Title = ({id,title}) => {
+const Title = ({id,title,pad}) => {
   return (
-    <Link to={`/product/${id}`} className='w-full px-2 py-2 flex items-center'>
+    <Link to={`/product/${id}`} className={`w-full ${pad} flex items-center`}>
       <h3 className='w-max font-medium line-clamp-1'>{title}</h3>
     </Link>
   )
 }
 
-const Price = ({price}) => <h4 className='px-1 text-xl text-yellow-500 font-semibold'>${price}</h4>
+const Price = ({price,pad}) => <h4 className={`${pad} text-xl text-yellow-500 font-semibold`}>${price}</h4>
 
-const Description = ({description}) => <p className='px-1 text-sm text-gray-500 line-clamp-2'>{description}</p>
+const Description = ({description,pad}) => <p className={`${pad} text-sm text-gray-500 line-clamp-2`}>{description}</p>
 
 const Brand = ({brand}) => <span className='px-1 py-1 text-sm text-gray-500 uppercase'>{brand}</span>
 
@@ -46,11 +46,26 @@ const Stock = ({stock}) => {
   )
 }
 
-const Location = ({location}) => <p className='max-w-btn px-1 py-1 text-sm text-gray-500 truncate' >{location}, Argentina</p>
+const Location = ({location,pad}) => <p className={`max-w-btn ${pad && pad} text-sm text-gray-500 truncate`} >{location}, Argentina</p>
 
-const Date = ({date}) => <p className='px-1 py-1 text-sm text-gray-500 truncate' >{date}</p>
+const Date = ({date,pad}) => <p className={`max-w-btn ${pad && pad} text-sm text-gray-500 truncate`} >{date}</p>
 
-export const ProductCard = ({content, style, size}) => {
+const CTA = ({product, qty, toggle, detail, pad}) => {
+  return(
+    <div className={`w-full ${pad} flex gap-2`}>
+      <BtnAddCart product={product} qty={qty} />
+      <BtnAddWishlist product={product} toggle={toggle !== 'l'}/>
+      <button 
+      className='w-8 h-8 flex items-center justify-center flex-none bg-yellow-500 text-white rounded-md'
+      onClick={() => detail(true)}
+             >
+        <i className="fa-solid fa-eye"></i>
+      </button>
+    </div>
+  )
+}
+
+export const ProductCard = ({content, size}) => {
   const [cardDetail, setCardDetail] = useState(false)
   const {id,name,thumb,price,description,province,timestamp,stock,brand} = content
   const [location, setLocation] = useState({})
@@ -61,62 +76,55 @@ export const ProductCard = ({content, style, size}) => {
     
   },[province])
 
-
-
-  if(size === 'm') return (
-    <>
-      <Card size='w-1/2' direction='row'>
-        <Thumb id={id} size='w-full max-w-xs h-40 h-full flex-none' thumb={thumb} name={name} />
-        <div className="px-2 py-2 flex flex-col grow divide-y divide-gray-300">
-          <Title id={id} title={name} />
-          <div className="px-1 pt-2 pb-1 flex flex-col">
-            <Brand brand={brand} />
-            <Description description={description} />
-            <div className="flex justify-between">
-              <Location location={location.nombre} />
-              <Date date={formatDate(timestamp.toDate())} />
-            </div>
-            <Price price={price} />
-            <Stock stock={stock} />
-          </div>
-        </div>
-      </Card>
-    </>
-  )
-
-
   return (
     <>
-      <article className={`w-full max-w-xs px-1 py-1 flex ${style === 'grid' ? 'flex-col items-center' : 'flex-row items-start'} flex-none bg-white border border-gray-300 rounded-md duration-150 hover:shadow-xl`}>
-        <Link to={`/product/${id}`} className='w-full max-w-xs h-56 px-1 py-1 flex items-center justify-center flex-none overflow-hidden'>
-          <img src={thumb} alt={name} className='h-full object-contain' />
-        </Link>
-        <div className="px-2 py-1 w-full h-full flex flex-col justify-between grow">
-          <div className={`flex flex-col ${style === 'list' && ''}`}>
-            <h4 className={`px-1 py-1 text-lg text-yellow-500 font-medium`}>${price}</h4>
-            <h3 className={`w-full px-1 py-1 flex font-medium`}>
-              <Link to={`/product/${id}`} className='w-max line-clamp-1'>{name}</Link>
-            </h3>
-            {style === 'list' && <p className='py-1 mb-2 text-sm text-gray-500 line-clamp-2'>{description}</p>}
-          </div>
-          <div className={`flex flex-col`}>
-            <p className='px-1 text-sm text-gray-500 truncate'>{location.nombre}, Argentina</p>
-            <p className='px-1 text-sm text-gray-500 truncate' >{formatDate(timestamp.toDate())}</p>
-          </div>
-          <div 
-          className={`w-full px-1 pt-2 flex gap-2 `}
-          >
-            <BtnAddCart product={content} qty={1} />
-            <BtnAddWishlist product={content} toggle={style === 'grid'}/>
-            <button 
-            className='w-8 h-8 flex items-center justify-center flex-none bg-yellow-500 text-white rounded-md'
-            onClick={() => setCardDetail(true)}
-            >
-              <i className="fa-solid fa-eye"></i>
-            </button>
-          </div>
-        </div>
-      </article>
+      <Card 
+      size={size === 's' ? 'w-full max-w-xs' : size === 'm' ? 'w-1/2' : 'w-full'} 
+      direction={size !== 's' ? 'row' : 'col'}
+      >
+        <Thumb 
+        id={id} 
+        size={`w-full max-w-xs ${size !== 'm' ? 'h-56' : 'h-full'}`} 
+        thumb={thumb} 
+        name={name} 
+        />
+        {size === 's' 
+        ? <>
+            <Title id={id} title={name} pad='px-1' />
+            <Price price={price} pad='px-1 py-1' />
+            <Location location={location.nombre} pad='px-1' />
+            <Date date={formatDate(timestamp.toDate())} pad='px-1' />
+            <CTA product={content} qty={1} toggle={size} detail={setCardDetail} pad='px-1 pt-2 pb-1' />
+          </>
+        : size === 'm'
+          ? <div className="px-2 py-2 flex flex-col grow divide-y divide-gray-300">
+              <Title id={id} title={name} pad='px-2 py-2' />
+              <div className="px-1 pt-2 pb-1 flex flex-col">
+                <Brand brand={brand} />
+                <Description description={description} pad='px-1 py-1' />
+                <div className="flex justify-between">
+                  <Location location={location.nombre} pad='px-1 py-1' />
+                  <Date date={formatDate(timestamp.toDate())} pad='px-1 py-1' />
+                </div>
+                <Price price={price} pad='px-1 py-1' />
+                <Stock stock={stock} />
+              </div>
+              <CTA product={content} qty={1} toggle={size} detail={setCardDetail} pad='px-2 pt-2' />
+            </div>
+          : <div className="flex flex-col grow divide-y divide-gray-300">
+              <Title id={id} title={name} pad='px-2 py-2' />
+              <div className="flex flex-col gap-2">
+                <Price price={price} pad='px-2 pt-2' />
+                <Description description={description} pad='px-2' />
+                <div className="w-full flex justify-between">
+                  <Location location={location.nombre} pad='px-2' />
+                  <Date date={formatDate(timestamp.toDate())} pad='px-2' />
+                </div>
+              </div>
+              <CTA product={content} qty={1} toggle={size} detail={setCardDetail} pad='px-2 pt-2' />
+            </div>
+        }
+      </Card>
       {cardDetail && <ProductModal content={content} handle={setCardDetail} />}
     </>
   )
