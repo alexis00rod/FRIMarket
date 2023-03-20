7c1c9fcbbbc416968175275f3ee164ee0198b77f
alexis00rod <alexis00rodrigo@gmail.com>
2023-03-20 11:49:09 -0300

    Agregue buscador de marca

--- src/components/PostBrand/PostBrand.jsx
+++ src/components/PostBrand/PostBrand.jsx
@@ -3,6 +3,7 @@ import { addBrand } from '../../services/firestore'
 
 export const PostBrand = ({selected,category, ...props}) => {
   const [brandToAdd, setBrandToAdd] = useState('')
+  const [searchBrand, setSearchBrand] = useState()
   const [dropDown, setDropDown] = useState(false)
 
   useEffect(() => {
@@ -20,6 +21,8 @@ export const PostBrand = ({selected,category, ...props}) => {
     setBrandToAdd('')
   }
 
+  const brands = searchBrand ? category.brands.filter(e => e.includes(searchBrand)) : category.brands
+
   return (
     <div className="px-2 py-2 flex flex-col">
       <span className="px-1 text-sm font-medium">Marca:</span>
@@ -29,20 +32,34 @@ export const PostBrand = ({selected,category, ...props}) => {
           <i className={`w-8 h-full flex items-center justify-center fa-solid fa-chevron-${dropDown ? 'up' : 'down'}`}></i>
         </button>
         {dropDown &&
-        <ul 
-        className='absolute top-full left-0 z-10 w-full h-max flex flex-col bg-white border-x border-b border-gray-300 rounded-b-md shadow-md overflow-hidden'>
-          {category.brands.map(e => (
-            <li key={e} className='w-full h-8' >
-              <input type="radio" name="brand" id={e} className='hidden' {...props} />
-              <label 
-              htmlFor={e} 
-              className='w-full h-full px-2 flex items-center text-sm capitalize hover:bg-gray-100 cursor-pointer'
-              >
-                {e}
-              </label>
-            </li>
-          ))}
-          <li className='w-full h-8 flex'>
+        <div className='absolute top-full left-0 z-10 w-full h-max max-h-dropdown flex flex-col bg-white border-x border-b border-gray-300 rounded-b-md shadow-md overflow-hidden'>
+          {/* Search brands */}
+          <div className="w-full h-8 flex flex-none">
+            <input 
+            type="text" 
+            name="searchBrand" 
+            className='px-2 grow outline-none placeholder:text-sm' 
+            placeholder='Buscar marca'
+            value={searchBrand}
+            onChange={({target:{value}}) => setSearchBrand(value)}
+            />
+          </div>
+          {/* Brands */}
+          <ul className="w-full flex flex-col grow overflow-y-scroll">
+            {brands.map(e => (
+              <li key={e} className='w-full h-8 flex-none' >
+                <input type="radio" name="brand" id={e} className='hidden' {...props} />
+                <label 
+                htmlFor={e} 
+                className='w-full h-full px-2 flex items-center text-sm capitalize hover:bg-gray-100 cursor-pointer'
+                >
+                  {e}
+                </label>
+              </li>
+            ))}
+          </ul>
+          {/* Add brand */}
+          <div className="w-full h-8 flex flex-none">
             <input 
             type="text" 
             name="" 
@@ -55,8 +72,8 @@ export const PostBrand = ({selected,category, ...props}) => {
             <button className='w-8 h-full hover:text-green-500' onClick={addNewBrand}>
               <i className="fa-solid fa-check"></i>
             </button>
-          </li>
-        </ul>}
+          </div>
+        </div>}
       </div>
     </div>
   )
