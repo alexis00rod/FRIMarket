d5fe6196ae232679e6fa6994c3e296313ac67e76
alexis00rod <alexis00rodrigo@gmail.com>
2023-03-21 13:15:11 -0300

    Agregue componente CategoriesMenu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { Link, NavLink } from "react-router-dom"
 import { useCartContext } from "../../context/CartContext/CartContext.jsx"
-import { CartMenu, Logo, Searchbox, UserMenu } from "../index.js"
+import { CartMenu, CategoriesMenu, Logo, Searchbox, UserMenu } from "../index.js"
 
 const NavbarLink = ({children,...props}) => {
   return (
@@ -16,11 +16,12 @@ const NavbarLink = ({children,...props}) => {
 export const Navbar = () => {
   const [cartMenu, setCartMenu] = useState(false)
   const [userMenu, setUserMenu] = useState(false)
+  const [categoriesMenu, setCategoriesMenu] = useState(false)
   const {cartQty} = useCartContext()
 
   return (
     <>
-      <header className="sticky top-0 left-0 w-full">
+      <header className="sticky top-0 left-0 z-30 w-full">
         <div className="w-full h-12 px-1 bg-white">
           <div className="w-full max-w-screen-2xl h-full px-1 mx-auto flex items-center">
             <Logo />
@@ -47,12 +48,20 @@ export const Navbar = () => {
         </div>
         <div className="w-full h-12 px-1 bg-blue-500 text-white">
           <nav className="w-full max-w-screen-2xl h-full mx-auto px-2 flex items-center gap-2">
+            <button 
+            className="h-full px-2 flex items-center gap-2 font-medium text-white"
+            onClick={() => setCategoriesMenu(!categoriesMenu)}
+            >
+              <i className="fa-solid fa-bars"></i>
+              Categorias
+            </button>
             <NavbarLink to='/'>Inicio</NavbarLink>
             <NavbarLink to='/shop/all'>Tienda</NavbarLink>
             <NavbarLink to='/sellers'>Vendedores</NavbarLink>
           </nav>
         </div>
       </header>
+      {categoriesMenu && <CategoriesMenu handle={setCategoriesMenu} />}
       {cartMenu && <CartMenu handle={setCartMenu} />}
       {userMenu && <UserMenu handle={setUserMenu} />}
     </>
