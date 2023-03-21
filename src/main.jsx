37ac2408c165d1b44b3caf929ae06b1ab4a381b2
alexis00rod <alexis00rodrigo@gmail.com>
2023-03-21 13:16:15 -0300

    Agregue contexto Shop Provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './components'
 import { AuthContextProvider, CartContextProvider } from './context'
+import { ShopContextProvider } from './context/ShopContext/ShopContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-        <CartContextProvider>
-          <App />
-        </CartContextProvider>
+        <ShopContextProvider>
+          <CartContextProvider>
+            <App />
+          </CartContextProvider>
+        </ShopContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
