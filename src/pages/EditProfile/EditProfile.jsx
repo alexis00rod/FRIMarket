b0dbfdfe8c5acdfc7614a77cafd5010ebcbf9338
alexis00rod <alexis00rodrigo@gmail.com>
2023-03-28 11:34:50 -0300

    Agregue funcion handleEditProfile

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { Breadcrumb, InputAddress, InputBio, InputCP, InputDisplayName, InputPhone, InputUserID, Loader, SelectCity, SelectProvince } from "../../components"
+import { Breadcrumb, InputAddress, InputBio, InputCP, InputDisplayName, InputPhone, InputPhoto, InputUserID, Loader, SelectCity, SelectProvince } from "../../components"
 import { useProfile } from "../../hooks/useProfile"
 import { updateProfileInfo } from "../../services/firestore"
 import { uploadUserPhoto } from "../../services/storage"
@@ -21,6 +21,18 @@ export const EditProfile = () => {
     navigate('/')
   }
 
+  const handleEditProfile = ({target:{name,value,id}}) => {
+    setEditProfile({
+      ...editProfile,
+      [name]: 
+          name === 'phone' || name === 'cp' 
+          ? parseInt(value) 
+          : name === 'province' || name === 'city'
+            ? id
+            : value
+    })
+  }
+
   return (
     <>
     <Breadcrumb />
@@ -37,27 +49,10 @@ export const EditProfile = () => {
               <h4 className="w-full px-2 py-1 font-semibold">Datos personales</h4>
               <div className="w-full flex flex-col">
                 <div className="w-full flex gap-2">
-                  <div className="px-2 py-2 flex flex-col">
-                    <span className="px-1 text-sm font-medium">Foto</span>
-                    <input 
-                    type="file" 
-                    name='photoURL' 
-                    id='photoURL' 
-                    className="hidden"
-                    onChange={handlePhoto}
-                    />
-                    <label 
-                    htmlFor='photoURL' 
-                    className="w-24 h-24 px-1 py-1 flex items-center justify-center flex-none border border-gray-300 rounded-md cursor-pointer"
-                    >
-                      {editProfile.photoURL
-                      ? <img src={editProfile.photoURL} alt={editProfile.idUser} className='w-full h-full object-cover' />
-                      : <img src={profile.photoURL} alt={profile.idUser} className='w-full h-full object-cover' />}
-                    </label>
-                  </div>
+                  <InputPhoto photo={profile.photoURL} newPhoto={editProfile.photoURL} name={profile.displayName} onChange={handlePhoto} />
                   <div className="flex flex-col grow">
-                    <InputDisplayName defaultValue={profile.displayName} onChange={({target: {name,value}}) => setEditProfile({...editProfile,[name]:value})} />
-                    <InputUserID defaultValue={profile.idUser} onChange={({target: {name,value}}) => setEditProfile({...editProfile,[name]:value})} />
+                    <InputDisplayName defaultValue={profile.displayName} onChange={handleEditProfile} />
+                    <InputUserID defaultValue={profile.idUser} onChange={handleEditProfile} />
                   </div>
                 </div>
                 <InputBio defaultValue={profile.bio} onChange={({target:{name,value}}) => setEditProfile({...editProfile,[name]:value})} />
@@ -69,25 +64,17 @@ export const EditProfile = () => {
             <div className="w-full py-2 flex flex-col">
               <h4 className="w-full px-2 py-1 font-semibold">Datos de envio</h4>
               <div className="w-full flex flex-col">
-                {/* Edit Province */}
-                {/* <SelectProvince 
-                selected={editProfile.province ? editProfile.province : province} 
-                onChange={({target: {name,id}}) => setEditProfile({...editProfile,[name]: id})} 
-                /> */}
-                {/* Edit City */}
-                {/* <SelectCity 
-                province={editProfile.province ? editProfile.province : province} 
-                selected={editProfile.city} 
-                onChange={({target: {name,id}}) => setEditProfile({...editProfile,[name]: id})} 
-                /> */}
-                {/* {province && 
+                <SelectProvince 
+                selected={editProfile.province ? editProfile.province : profile.province} 
+                onChange={handleEditProfile} 
+                />
                 <SelectCity 
-                province={province} 
-                selected={city} 
-                onChange={({target: {name,id}}) => setEditProfile({...editProfile,[name]: id})} 
-                />} */}
-                <InputAddress defaultValue={profile.address} onChange={({target: {name,value}}) => setEditProfile({...editProfile,[name]:value})} />
-                <InputCP defaultValue={profile.cp} onChange={({target: {name,value}}) => setEditProfile({...editProfile,[name]:value})} />
+                selected={editProfile.city ? editProfile.city : profile.city} 
+                province={editProfile.province ? editProfile.province : profile.province} 
+                onChange={handleEditProfile} 
+                />
+                <InputAddress defaultValue={profile.address} onChange={handleEditProfile} />
+                <InputCP defaultValue={profile.cp} onChange={handleEditProfile} />
               </div>
             </div>
             <button type="submit" className="w-full max-w-btn h-8 px-2 ml-2 flex items-center justify-center gap-2 bg-green-500 text-white rounded-md">
