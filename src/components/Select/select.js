c710ef10808d00f831731c1c6bb012afde261f3d
alexis00rod <alexis00rodrigo@gmail.com>
2023-03-30 23:30:57 -0300

    Agregue componentes SelectSearch y SelectCity

--- src/components/Select/select.js
+++ src/components/Select/select.js
@@ -1,6 +1,7 @@
-import { Select, SelectItem } from "./Select.jsx"
+import { Select, SelectItem, SelectSearch } from "./Select.jsx"
+import { SelectCity } from "./SelectCity.jsx"
 import { SelectProductsSort } from "./SelectProductsSort.jsx"
 import { SelectProvince } from "./SelectProvince.jsx"
 import { SelectSellersSort } from "./SelectSellersSort.jsx"
 
-export {Select, SelectItem, SelectProvince, SelectSellersSort, SelectProductsSort}
+export {Select, SelectItem, SelectProvince, SelectSellersSort, SelectProductsSort, SelectCity, SelectSearch}
