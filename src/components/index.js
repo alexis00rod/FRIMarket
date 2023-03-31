3758dd1d743b894a28758a34db685fde8b11fb72
alexis00rod <alexis00rodrigo@gmail.com>
2023-03-30 23:48:39 -0300

    Agregue componentes SelectCity, SelectSearch y form

--- src/components/index.js
+++ src/components/index.js
@@ -1,58 +1,47 @@
-import { Accordion } from "./Accordion/Accordion";
-import { App } from "./App/App";
-import { CartProduct } from "./CartProduct/CartProduct";
-import { Footer } from "./Footer/Footer";
-import { Loader } from "./Loader/Loader";
-import { PostBrand } from "./PostBrand/PostBrand";
-import { PostType } from "./PostType/PostType";
-import { ProductModal } from "./ProductModal/ProductModal";
-import { PostThumb } from "./PostThumb/PostThumb";
-import { PostCategory } from "./PostCategory/PostCategory";
-import { PostName } from "./PostName/PostName";
-import { PostDescription } from "./PostDescription/PostDescription";
-import { PostPrice } from "./PostPrice/PostPrice";
-import { PostStock } from "./PostStock/PostStock";
-import { SignupName } from "./SignupName/SignupName";
-import { SignupEmail } from "./SignupEmail/SignupEmail";
-import { SignupPass } from "./SignupPass/SignupPass";
-import { LoginEmail } from "./LoginEmail/LoginEmail";
-import { LoginPass } from "./LoginPass/LoginPass";
-import { ProfileInfo } from "./ProfileInfo/ProfileInfo";
-import { SignupUserID } from "./SignupUserID/SignupUserID";
-import { SignupPhoto } from "./SignupPhoto/SignupPhoto";
-import { ProtectedRoutes } from "./ProtectedRoutes/ProtectedRoutes";
-import { Searchbox } from "./Searchbox/Searchbox";
-import { ProductsDetailSimilar } from "./ProductDetailSimilar/ProductDetailSimilar";
-import { ProductDetailReviews } from "./ProductDetailReviews/ProductDetailReviews";
-import { ProfilePosts } from "./ProfilePosts/ProfilePosts";
-import { EditProduct } from "./EditProduct/EditProduct";
-import { SignupLocation } from "./SignupLocation/SignupLocation";
-import { ShopFiltersMenu } from "./ShopFiltersMenu/ShopFiltersMenu";
-import { HomeTabs } from "./HomeTabs/HomeTabs";
-import { HomeHero } from "./HomeHero/HomeHero";
-import { HomeSpecialProducts } from "./HomeSpecialProducts/HomeSpecialProducts";
-import { Logo } from "./Logo/Logo";
-import { Slider } from "./Slider/Slider";
-import { Breadcrumb, BreadcrumbLink } from "./Breadcrumb/Breadcrumb";
-import { SelectCity } from "./SelectCity/SelectCity";
-import { InputDisplayName } from "./InputDisplayName/InputDisplayName";
-import { InputEmail } from "./InputEmail/InputEmail";
-import { InputPhone } from "./InputPhone/InputPhone";
-import { InputAddress } from "./InputAddress/InputAddress";
-import { InputCP } from "./InputCP/InputCP";
-import { InputUserID } from "./InputUserID/InputUserID";
-import { InputBio } from "./InputBio/InputBio";
-import { InputPhoto } from "./InputPhoto/InputPhoto";
+import { Accordion } from "./Accordion/Accordion"
+import { App } from "./App/App"
+import { CartProduct } from "./CartProduct/CartProduct"
+import { Footer } from "./Footer/Footer"
+import { Loader } from "./Loader/Loader"
+import { PostBrand } from "./PostBrand/PostBrand"
+import { PostType } from "./PostType/PostType"
+import { ProductModal } from "./ProductModal/ProductModal"
+import { PostThumb } from "./PostThumb/PostThumb"
+import { PostCategory } from "./PostCategory/PostCategory"
+import { PostName } from "./PostName/PostName"
+import { PostDescription } from "./PostDescription/PostDescription"
+import { PostPrice } from "./PostPrice/PostPrice"
+import { PostStock } from "./PostStock/PostStock"
+import { ProfileInfo } from "./ProfileInfo/ProfileInfo"
+import { ProtectedRoutes } from "./ProtectedRoutes/ProtectedRoutes"
+import { Searchbox } from "./Searchbox/Searchbox"
+import { ProductsDetailSimilar } from "./ProductDetailSimilar/ProductDetailSimilar"
+import { ProductDetailReviews } from "./ProductDetailReviews/ProductDetailReviews"
+import { ProfilePosts } from "./ProfilePosts/ProfilePosts"
+import { EditProduct } from "./EditProduct/EditProduct"
+import { ShopFiltersMenu } from "./ShopFiltersMenu/ShopFiltersMenu"
+import { HomeTabs } from "./HomeTabs/HomeTabs"
+import { HomeHero } from "./HomeHero/HomeHero"
+import { HomeSpecialProducts } from "./HomeSpecialProducts/HomeSpecialProducts"
+import { Logo } from "./Logo/Logo"
+import { Slider } from "./Slider/Slider"
+import { Breadcrumb, BreadcrumbLink } from "./Breadcrumb/Breadcrumb"
+import { InputDisplayName } from "./InputDisplayName/InputDisplayName"
+import { InputPhone } from "./InputPhone/InputPhone"
+import { InputAddress } from "./InputAddress/InputAddress"
+import { InputCP } from "./InputCP/InputCP"
+import { InputUserID } from "./InputUserID/InputUserID"
+import { InputBio } from "./InputBio/InputBio"
 
 import { Navbar, NavbarMenu, CartMenu, UserMenu } from './Navbar/navbar'
 import { Button, BtnShare, BtnAddCart, BtnAddWishlist } from './Button/button'
-import { Modal } from "./Modal/Modal";
-import { DetailImage, DetailTitle, DetailLocation, DetailDate, DetailDescription, DetailUser, DetailCTA } from "./Detail/detail";
-import { Reviews, ReviewsSort, ReviewRating, ReviewCard } from "./Reviews/reviews";
-import { InputText, Textarea, InputRating } from "./Form/form";
-import { SellerCard, ProductCard } from "./Card/card";
-import { Select, SelectItem, SelectProductsSort, SelectProvince, SelectSellersSort } from "./Select/select";
-import { ProductsList, ProductsLayout } from "./Products/products";
+import { Modal } from "./Modal/Modal"
+import { DetailImage, DetailTitle, DetailLocation, DetailDate, DetailDescription, DetailUser, DetailCTA } from "./Detail/detail"
+import { Reviews, ReviewsSort, ReviewRating, ReviewCard } from "./Reviews/reviews"
+import { InputText, Textarea, InputRating, InputEmail, InputPassword, InputPhoto } from "./Form/form"
+import { SellerCard, ProductCard } from "./Card/card"
+import { Select, SelectCity, SelectItem, SelectProductsSort, SelectProvince, SelectSearch, SelectSellersSort } from "./Select/select"
+import { ProductsList, ProductsLayout } from "./Products/products"
 
 export {
   // Main
@@ -68,15 +57,13 @@ export {
   // Cards
   ProductCard, SellerCard,
   // Select
-  Select, SelectItem, SelectProvince, SelectSellersSort, SelectProductsSort,
+  Select, SelectItem, SelectProvince, SelectSellersSort, SelectProductsSort, SelectCity, SelectSearch,
   // Products
   ProductsList, ProductsLayout,
   // Form
-  InputText, Textarea, InputRating,
+  InputText, Textarea, InputRating, InputEmail, InputPassword, InputPhoto,
   // Form
-  InputDisplayName, InputEmail, InputPhone, InputAddress, InputCP, InputUserID, InputBio, InputPhoto,
-  // Select
-  SelectCity,
+  InputDisplayName, InputPhone, InputAddress, InputCP, InputUserID, InputBio,
   // Breadcrumb
   Breadcrumb, BreadcrumbLink,
   // Home
@@ -93,8 +80,4 @@ export {
   ProductsDetailSimilar, ProductDetailReviews, EditProduct,
   // Profile
   ProfileInfo, ProfilePosts,
-  // Signup
-  SignupUserID, SignupName, SignupPhoto, SignupEmail, SignupPass, SignupLocation,
-  // Login
-  LoginEmail, LoginPass,
 }
