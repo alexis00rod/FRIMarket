0da6b3661244b4c9b5a50a5432af0fa57a385e20
alexis00rod <alexis00rodrigo@gmail.com>
2023-04-01 12:16:06 -0300

    Agregue componente Hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
-import { HomeHero, HomeSpecialProducts } from "../../components"
+import { Hero, HomeHero, HomeSpecialProducts } from "../../components"
 import { HomeTabs } from "../../components/HomeTabs/HomeTabs"
 
 export const Home = () => {
   return (
     <main>
       <section className="w-full flex flex-col gap-2 md:gap-4">
-        <HomeHero />
+        <Hero />
+        {/* <HomeHero /> */}
         <HomeTabs />
         <HomeSpecialProducts />
       </section>
