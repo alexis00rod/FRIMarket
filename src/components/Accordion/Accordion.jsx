2bf13b9d5e66a4384c94b31320e2cf94cef5a1f5
alexis00rod <alexis00rodrigo@gmail.com>
2023-04-01 11:00:53 -0300

    Agregue clase accordion

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -4,20 +4,14 @@ export const Accordion = ({title,open = true,children}) => {
   const [accordion, setAccordion] = useState(open)
 
   return (
-    <div className="w-full flex flex-col">
-      <div 
-        className={`w-full px-1 py-1 flex items-center ${accordion && 'text-blue-500'} cursor-pointer hover:text-blue-500`}
-        onClick={() => setAccordion(!accordion)}
-      >
-        <h2 className="px-1 grow font-medium">{title}</h2>
-        <button className="w-8 h-8 flex justify-center items-center">
-          <i className={`fa-solid fa-${accordion ? 'minus' : 'plus'}`}></i>
-        </button>
-      </div>
-      {accordion &&
-      <div className="w-full px-2 pb-2 flex flex-col">
+    <div className="accordion">
+      <button className="accordion-handle" onClick={() => setAccordion(!accordion)}>
+        <span className='accordion-title'>{title}</span>
+        <i className={`accordion-arrow ${accordion && '-rotate-180'} fa-solid fa-chevron-down`}></i>
+      </button>
+      <div className={`accordion-expand ${accordion && 'h-full py-2 opacity-100'}`}>
         {children}
-      </div>}
+      </div>
     </div>
   )
 }
