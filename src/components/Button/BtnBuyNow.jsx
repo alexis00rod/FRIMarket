b3df10dae9c9e89434a283e0be4c719167f32fbc
alexis00rod <alexis00rodrigo@gmail.com>
2023-04-04 19:07:27 -0300

    Agrege componente BtnBuyNow

---
+++ src/components/Button/BtnBuyNow.jsx
@@ -0,0 +1,7 @@
+import { Button } from "../index.js"
+
+export const BtnBuyNow = () => {
+  return (
+    <Button icon='check' color='btn-green' size='btn-m' ><span className="text-sm font-medium">Comprar ahora</span></Button>
+  )
+}
--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -2,5 +2,6 @@ import { Button } from "./Button.jsx";
 import { BtnShare } from "./BtnShare.jsx";
 import { BtnAddCart } from "./BtnAddCart.jsx";
 import { BtnAddWishlist } from "./BtnAddWishlist.jsx";
+import { BtnBuyNow } from "./BtnBuyNow.jsx";
 
-export {Button, BtnShare, BtnAddCart, BtnAddWishlist}
+export {Button, BtnShare, BtnAddCart, BtnAddWishlist, BtnBuyNow}
