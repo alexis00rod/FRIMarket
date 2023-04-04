8cc06f18965643d749c41e3a01643353dd1687b4
alexis00rod <alexis00rodrigo@gmail.com>
2023-04-04 19:15:27 -0300

    Agregue componete DetailCategory

--- src/components/Detail/detail.js
+++ src/components/Detail/detail.js
@@ -1,4 +1,5 @@
 import { DetailCTA } from "./DetailCTA.jsx";
+import { DetailCategory } from "./DetailCategory.jsx";
 import { DetailDate } from "./DetailDate.jsx";
 import { DetailDescription } from "./DetailDescription.jsx";
 import { DetailImage } from "./DetailImage.jsx";
@@ -6,4 +7,4 @@ import { DetailLocation } from "./DetailLocation.jsx";
 import { DetailTitle } from "./DetailTitle.jsx";
 import { DetailUser } from "./DetailUser.jsx";
 
-export {DetailImage, DetailTitle, DetailLocation, DetailDate, DetailDescription, DetailUser, DetailCTA}
+export {DetailImage, DetailTitle, DetailLocation, DetailDate, DetailDescription, DetailUser, DetailCTA, DetailCategory}
