7685b62df7fc1b4ead1be51b3cc42ee43281d86c
alexis00rod <alexis00rodrigo@gmail.com>
2023-04-07 03:27:01 -0300

    Agregue clase sellerCard

--- src/components/Card/SellerCard.jsx
+++ src/components/Card/SellerCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import { useUserPosts } from "../../hooks/useUserPosts"
 
 export const SellerCard = ({seller}) => {
-  const {email, idUser, photoURL, displayName, bio, posts, sales} = seller
+  const {email, idUser, photoURL, displayName, sales} = seller
   const {userPosts} = useUserPosts(email)
 
   return (
@@ -10,19 +10,17 @@ export const SellerCard = ({seller}) => {
       {/* Photo */}
       <img src={photoURL} alt={displayName} className="sellerCard-photo" />
       <div className="sellerCard-profile">
-        <div className="flex flex-col">
+        <div className="flex flex-col grow">
           {/* Name */}
           <h3 className='sellerCard-name'>{displayName}</h3>
           {/* IDUser */}
           <h4 className='sellerCard-id'>@{idUser}</h4>
-          {/* Bio */}
-          {bio && <p className='sellerCard-bio'>{bio}</p>}
         </div>
         <div className="flex flex-col">
           {/* Posts */}
-          <p className="sellerCard-posts">Publicaciones: <span className="font-medium">{userPosts.length}</span></p>
+          <p className="sellerCard-posts">Publicaciones: <span>{userPosts.length}</span></p>
           {/* Sales */}
-          <p className="sellerCard-sales">Ventas: <span className="font-medium">{sales ? sales : 0}</span></p>
+          <p className="sellerCard-sales">Ventas: <span>{sales ? sales : 0}</span></p>
         </div>
       </div>
     </Link>
