d772e3f9147a5b36510811f79668d2cfad93a577
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-01-25 18:42:44 -0300

    fix: browserRouter alias

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext/AuthContext'
 import { CartContextProvider } from './context/CartContext/CartContext'
 import { ShopContextProvider } from './context/ShopContext/ShopContext'
 import { App } from './components'
 import './index.css'
+import { RegistrationContextProvider } from './context/RegistrationContext/RegistrationContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthContextProvider>
-        <ShopContextProvider>
-          <CartContextProvider>
-            <App />
-          </CartContextProvider>
-        </ShopContextProvider>
-      </AuthContextProvider>
-    </BrowserRouter>
+    <Router>
+      <RegistrationContextProvider>
+        <AuthContextProvider>
+          <ShopContextProvider>
+            <CartContextProvider>
+              <App />
+            </CartContextProvider>
+          </ShopContextProvider>
+        </AuthContextProvider>
+      </RegistrationContextProvider>
+    </Router>
   </React.StrictMode>,
 )
