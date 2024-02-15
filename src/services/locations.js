ebc10b6f9363cfdeaa8dd88ef9e3fc44a60f07ac
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-02-15 15:49:29 -0300

    add: get locations to firebase

--- src/services/locations.js
+++ src/services/locations.js
@@ -1,3 +1,6 @@
+import { getDocs } from "firebase/firestore"
+import { locationsRef } from "./firestore"
+
 export const getProvinces = async () => {
   const request = await fetch(`https://apis.datos.gob.ar/georef/api/provincias`)
   const json = await request.json()
@@ -6,8 +9,14 @@ export const getProvinces = async () => {
 }
 
 export const getCities = async (province) => {
+  // console.log(province)
   const request = await fetch(`https://apis.datos.gob.ar/georef/api/departamentos?provincia=${province}&max=999`)
   const json = await request.json()
   const {departamentos} = json
   return departamentos
+}
+
+export const getLocations = async () => {
+  const locations = await getDocs(locationsRef)
+  return locations.docs
 }
