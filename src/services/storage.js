2aac8732da25adcf0acc63e6be6ec96e9a70272e
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-02-15 15:45:20 -0300

    add: function for upload and delete images in product to post

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,9 +1,10 @@
-import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage'
+import { deleteObject, getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage'
 
 const storage = getStorage()
 
 // Referencias
 const productThumbRef = (user,name) => ref(storage, `${user}/products/${name}`)
+const productToPostImageRef = (user,name) => ref(storage, `${user}/products/${name}`)
 const userPhotoRef = (user,name) => ref(storage, `${user}/photo/${name}`)
 
 export const uploadThumb = async (user,file) => {
@@ -22,4 +23,15 @@ export const uploadUserPhoto = async (user,file) => {
   await uploadBytes(userPhotoRef(email,name),file)
   const url = await getDownloadURL(userPhotoRef(email,name))
   return url
+}
+
+export const uploadProductToPostImage = async (user,file) => {
+  await uploadBytes(productToPostImageRef(user, file.name),file)
+  const url = await getDownloadURL(productToPostImageRef(user, file.name))
+  return {name: file.name, url}
+}
+
+export const deleteProductToPostImage = async (user, name) => {
+  await deleteObject(productToPostImageRef(user, name))
+  return name
 }
