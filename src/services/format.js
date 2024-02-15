1e905ec8d2fe59a8d5fe8a31b6e413aa30013153
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-02-15 15:48:50 -0300

    add: format price and format phone

--- src/services/format.js
+++ src/services/format.js
@@ -12,3 +12,18 @@ export const generateIdUser = (user) => {
   const id = ((Math.floor(Math.random() * 10000) + 1).toString())
   return (nameUser+id).toLowerCase()
 }
+
+export const formatPrice = (price) => {
+  price = price.replace(/[^\d,]/g, "")
+  let parts = price.split(",")
+  if (parts[0] === "" && parts[1] !== undefined && parts[1] !== "") {
+    parts[0] = "0"
+  }
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+  if (parts[1] && parts[1].length > 2) {
+    parts[1] = parts[1].slice(0, 2)
+  }
+  return parts.join(",")
+}
+
+export const formatPhone = (phone) => phone.replace(/[^\d]/g, "")
