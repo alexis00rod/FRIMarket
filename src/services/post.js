dd80be2782b98acd4f37292d7d6442fdb2c5351e
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-02-15 15:47:41 -0300

    fix:function addProduct: add default image if !images.length

--- src/services/post.js
+++ src/services/post.js
@@ -2,10 +2,14 @@ import { addDoc, serverTimestamp, updateDoc, } from 'firebase/firestore'
 import { categoryRef, productsRef, userRef } from "./firestore";
 
 // Funcion para agregar producto
-// Devolver notificacion
 export const addProduct = async (product) => {
+  const defaultPhoto = 'https://firebasestorage.googleapis.com/v0/b/frimarket-f4864.appspot.com/o/default-noimage.jpg?alt=media&token=8f0492c4-15e3-44d5-980a-e799fc0d5a93'
+  const images = product.images.length 
+    ? product.images 
+    : [{name: 'default-noimage.jpg',url:defaultPhoto}]
   return await addDoc(productsRef, {
     ...product,
+    images: images,
     date: serverTimestamp()
   })
 }
