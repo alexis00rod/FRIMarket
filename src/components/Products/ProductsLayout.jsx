83435cf6cc3539e950f01d03cd59b3cc127995b1
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-02-19 17:19:18 -0300

    fix: style

--- src/components/Products/ProductsLayout.jsx
+++ src/components/Products/ProductsLayout.jsx
@@ -1,10 +1,12 @@
-import { Button } from "../index.js"
-
 export const ProductsLayout = ({size,handle}) => {
   return (
     <div className="w-max px-2 flex items-center gap-2">
-      <Button icon='table-cells' color={size === 's' ? 'btn-blue' : 'btn-white'} size='btn-s' onClick={() => handle('s')} />
-      <Button icon='list' color={size === 'l' ? 'btn-blue' : 'btn-white'} size='btn-s' onClick={() => handle('l')} />
+      <button className={`btn btn-s btn-${size === 's' ? 'blue' : 'white'}`} onClick={() => handle('s')}>
+        <i className="fa-solid fa-table-cells"></i>
+      </button>
+      <button className={`btn btn-s btn-${size === 'l' ? 'blue' : 'white'}`} onClick={() => handle('l')}>
+        <i className="fa-solid fa-list"></i>
+      </button>
     </div>
   )
 }
