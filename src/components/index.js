d32b1541a6dbc8543176bcf9905924ee03b12de0
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-02-19 17:20:19 -0300

    add: products sort component

--- src/components/index.js
+++ src/components/index.js
@@ -2,13 +2,12 @@ import { App } from "./App/App"
 import { ProtectedRoutes } from "./ProtectedRoutes/ProtectedRoutes"
 import { Navbar, NavbarMenu, CartMenu, UserMenu } from './Navbar/navbar'
 import { Button, BtnShare, BtnAddCart, BtnAddWishlist, BtnBuyNow, ButtonLink } from './Button/button'
-import { Modal, EditProduct, ProductDetail, ShopFilter } from "./Modal/modal"
+import { Modal, EditProduct, ProductDetail } from "./Modal/modal"
 import { DetailImage, DetailTitle, DetailLocation, DetailDescription, DetailCTA, DetailCategory } from "./Detail/detail"
 import { Reviews, ReviewRating } from "./Reviews/reviews"
 import { InputText, Textarea, InputRating, InputEmail, InputPassword, InputPhoto, InputNumber, InputSearch, InputProductQty } from "./Form/form"
 import { SellerCard, ProductCard, CategoryCard, CartProductCard, ReviewCard } from "./Card/card"
 import { Select, SelectAddItem, SelectCity, SelectItem, SelectProductBrand, SelectProductType, SelectProductsSort, SelectProvince, SelectSearch, SelectSellersSort, SelectReviewsSort, SelectOrderSort } from "./Select/select"
-import { ProductsList, ProductsLayout } from "./Products/products"
 import { PostProductDetail, PostProductLocation, PostProductPrice, PostProductThumb, PostProductUser, PostProductCategory } from "./PostProduct/postProduct"
 import { Logo } from "./Logo/Logo"
 import { Breadcrumb, BreadcrumbLink } from "./Breadcrumb/Breadcrumb"
@@ -19,8 +18,12 @@ import { Footer } from "./Footer/Footer"
 import { CheckoutInformation, CheckoutPayment, CheckoutShipping } from "./Checkout/checkout"
 import { EditProfileInfo, EditProfileShipping, ProfileInfo, ProfilePosts } from "./Profile/profile"
 import { SignupUser, SignupProfile } from "./Signup/signup"
+// Products
+import { ProductsList, ProductsLayout, ProductsSort } from "./Products/productsComponents"
 // Post
 import { PostBrand, PostCategory, PostChangeCategory, PostCondition, PostDescription, PostImages, PostPrice, PostShipping, PostStock, PostTitle, PostType, PostUserEmail, PostUserLocation, PostUserName, PostUserPhone } from "../pages/PostPage/components/postComponents"
+// Shop
+import { ShopFilter } from '../pages/ShopPage/components/shopComponents'
 
 export {
   App, Logo, Accordion, Loader, ProtectedRoutes, Element, Main, Footer, Menu,
@@ -36,16 +39,12 @@ export {
   ProductCard, SellerCard, CategoryCard, CartProductCard, ReviewCard,
   // Select
   Select, SelectItem, SelectProvince, SelectSellersSort, SelectProductsSort, SelectCity, SelectSearch, SelectProductBrand, SelectAddItem, SelectProductType, SelectReviewsSort, SelectOrderSort,
-  // Products
-  ProductsList, ProductsLayout,
   // Form
   InputText, Textarea, InputRating, InputEmail, InputPassword, InputPhoto, InputNumber, InputSearch, InputProductQty,
   // Post
   PostProductDetail, PostProductLocation, PostProductPrice, PostProductThumb, PostProductUser, PostProductCategory, 
-  // Post new
-  PostTitle, PostCategory, PostType, PostImages, PostDescription, PostCondition, PostStock, PostChangeCategory, PostBrand, PostPrice, PostShipping, PostUserName, PostUserEmail, PostUserPhone, PostUserLocation,
   // Modal
-  Modal, EditProduct, ProductDetail, ShopFilter,
+  Modal, EditProduct, ProductDetail,
   // Slider
   Slider, Hero, CategorySlider, ProductsSlider, SpecialsSlider, SimilarSlider,
   // Breadcrumb
@@ -56,4 +55,11 @@ export {
   CheckoutInformation, CheckoutShipping, CheckoutPayment,
   // Signup
   SignupUser, SignupProfile,
+
+  // Products
+  ProductsList, ProductsLayout, ProductsSort,
+  // Post
+  PostTitle, PostCategory, PostType, PostImages, PostDescription, PostCondition, PostStock, PostChangeCategory, PostBrand, PostPrice, PostShipping, PostUserName, PostUserEmail, PostUserPhone, PostUserLocation,
+  // Shop
+  ShopFilter
 }
