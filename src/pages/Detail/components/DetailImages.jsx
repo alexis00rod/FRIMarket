23a3c94ad1551ae2142111f1a6fe932e0c122693
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-02-21 16:34:02 -0300

    add: swiper and image magnifier

--- src/pages/Detail/components/DetailImages.jsx
+++ src/pages/Detail/components/DetailImages.jsx
@@ -1,30 +1,64 @@
 import { useState } from "react"
+import { ImageMagnifier } from "../../../components"
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
 
 export const DetailImages = ({images}) => {
   const [imageSelected, setImageSelected] = useState(images[0])
-
+  const [currentSlide, setCurrentSlide] = useState(1);
   return (
-    <div className="w-full p-2 flex flex-col lg:flex-row-reverse bg-white border border-slate-300 rounded-md">
-      <div className="p-2 grow flex items-center justify-center">
-        <img
-          src={imageSelected.url}
-          alt={imageSelected.name}
-          className="h-[500px] rounded-md"
-        />
+    <div className="w-full p-2 flex bg-white border border-slate-300 rounded-md">
+      {/* Lista images mobile */}
+      <div className="relative w-full h-[400px] lg:hidden">
+        <span className="absolute top-2 left-2 z-20 w-max px-2 py-1 text-sm font-medium bg-slate-300 rounded-full">{currentSlide} / {images.length}</span>
+        <Swiper
+        slidesPerView={1}
+        className="w-full h-full"
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        onSlideChange={(swiper) => setCurrentSlide(swiper.activeIndex + 1)}
+        >
+          {images.map((img,i) => (
+            <SwiperSlide
+            key={i}
+            className="flex items-center justify-center"
+            >
+              <img src={img.url} alt={img.name} className="w-full h-full object-contain" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-      <div className="w-full lg:w-max p-2 flex lg:flex-col justify-center lg:justify-start flex-none gap-2">
-        {images.map((img, i) => (
-          <img
-          key={i}
-          src={img.url}
-          alt={img.name}
-          className={`
-          w-20 h-20 object-cover rounded-md cursor-pointer
-          ${imageSelected.url === img.url ? 'border-4 border-blue-500' : 'border-2 border-slate-300'}
-          `}
-          onMouseOver={() => setImageSelected(img)}
-          />
-        ))}
+      {/* Lista images desktop */}
+      <div className="w-full hidden lg:flex h-[500px]">
+        <div className="w-max p-2 flex flex-col flex-none gap-2">
+          {images.slice(0,5).map((img, i) => (
+            <figure
+            key={i}
+            className={`
+            relative w-20 h-20 flex flex-none border-2 cursor-pointer rounded-md overflow-hidden
+            ${imageSelected.url === img.url ? 'border-blue-500' : 'border-slate-300'}
+            `}
+            onMouseOver={() => setImageSelected(img)}
+            onClick={() => setImageSelected(img)}
+            >
+              {i === 4
+              ? <>
+                  <img src={img.url} alt="" className="w-full h-full object-cover"/>
+                  <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-white/80">
+                    <span className="font-medium text-3xl text-blue-500">+ {images.length - 4}</span>
+                  </div>
+                </>
+              : <img src={img.url} alt="" className="w-full h-full object-cover"/>}
+            </figure>
+          ))}
+        </div>
+        <div className="p-2 grow flex items-center justify-center">
+          <ImageMagnifier image={imageSelected} />
+        </div>
       </div>
     </div>
   )
