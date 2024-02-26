41b090fa692356e865d1db1cb8572f723811ecdf
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-02-26 18:49:08 -0300

    add: formatPrice

--- src/pages/Detail/components/DetailPrice.jsx
+++ src/pages/Detail/components/DetailPrice.jsx
@@ -1,5 +1,7 @@
+import { formatPrice } from '../../../services/format'
+
 export const DetailPrice = ({price}) => {
   return (
-    <h3 className='px-2 mb-2 text-3xl text-yellow-500 font-medium'>${price}</h3>
+    <h3 className='px-2 mb-2 text-3xl text-yellow-500 font-medium'>${formatPrice(price)}</h3>
   )
 }
