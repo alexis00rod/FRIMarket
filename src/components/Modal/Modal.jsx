a6223a1fd2936d4ff18fda6b383505c1ffe82a73
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-02-27 18:50:47 -0300

    fix: press tab key

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,38 @@
+import { useEffect, useRef } from 'react';
 import { Button } from '../index.js'
 
 export const Modal = ({title, handle, direction, size, children}) => {
+  const modalRef = useRef(null)
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Tab') {
+        const focusableElements = modalRef.current.querySelectorAll(
+          'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+        )
+        const firstElement = focusableElements[0]
+        const lastElement = focusableElements[focusableElements.length - 1]
+
+        if (!event.shiftKey && document.activeElement === lastElement) {
+          firstElement.focus()
+          event.preventDefault()
+        } else if (event.shiftKey && document.activeElement === firstElement) {
+          lastElement.focus()
+          event.preventDefault()
+        }
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
   return (
     <div className={`modal ${direction}`} onClick={() => handle(false)}>
-      <div className={`modal-container ${size}`} onClick={e => e.stopPropagation()}>
+      <div className={`modal-container ${size}`} onClick={e => e.stopPropagation()} ref={modalRef}>
         <div className="modal-header">
           <h3>{title}</h3>
           <Button icon='x' color='btn-gray' size='btn-s' onClick={() => handle(false)} />
--- src/components/Modal/modal.js
+++
@@ -1,4 +0,0 @@
-import { Modal } from "./Modal.jsx";
-import { ProductDetail } from "./ProductDetail.jsx";
-
-export {Modal, ProductDetail}
