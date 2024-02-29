fa470045fb8902aa4b527495ecde876e78151de3
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-02-29 15:55:18 -0300

    add: updateUserEmail function

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,5 @@
 import { app } from "./firebase";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth'
+import { EmailAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateEmail, updateProfile } from 'firebase/auth'
 
 export const auth = getAuth(app)
 
@@ -20,9 +20,16 @@ export const stateAuth = (set) => {
 export const logout = () => signOut(auth)
 
 // Funcion para actualizar displayName
-export const updateDisplayName = async ({name, lastName}) => {
+export const updateUserDisplayName = async ({name, lastName}) => {
   const user = auth.currentUser
   await updateProfile(user,{
     displayName: `${name} ${lastName}`
   })
+}
+
+export const updateUserEmail = async (newEmail) => {
+  const user = auth.currentUser
+  if(user) {
+    await updateEmail(user,newEmail)
+  }
 }
