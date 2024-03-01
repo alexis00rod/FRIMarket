2cd98e4a1668aa1b0c78a776308134c317a26830
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-03-01 20:23:35 -0300

    add: input-checkbox class

--- src/components/Form/InputCheckbox.jsx
+++ src/components/Form/InputCheckbox.jsx
@@ -1,19 +1,16 @@
 export const InputCheckbox = ({name, id, checked,children, ...props}) => {
+
   return (
-    <div className="px-1 flex">
+    <label htmlFor={id} className="input-checkbox">
       <input
-      type="checkbox"
-      name={name}
-      id={id}
-      checked={checked}
-      {...props}
-    />
-    <label
-      htmlFor={id}
-      className="pl-2 flex items-center text-sm font-light cursor-pointer"
-    >
-      {children}
+        type="checkbox"
+        name={name}
+        id={id}
+        checked={checked}
+        {...props}
+      />
+      <span className="input-checkbox-checkmark"></span>
+      <span className='input-checkbox-name'>{children}</span>
     </label>
-    </div>
   )
 }
