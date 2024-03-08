e8c71474f3cc5752120808a2c9baf5b9239aa85e
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-03-08 14:48:08 -0300

    add: login class

--- src/pages/Login/EnterPass.jsx
+++ src/pages/Login/EnterPass.jsx
@@ -1,69 +1,66 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
-import { useLoginContext } from "../../context/LoginContext/LoginContext"
+import { useLoginContext } from "./context/LoginContext"
 import { loginEmailPass } from "../../services/auth"
-import { InputPassword } from "../../components"
+import { ButtonLoader, InputPassword, Notification } from "../../components"
 
 export const EnterPass = () => {
   const {userToLogin} = useLoginContext()
   const {email, photoURL, idUser} = userToLogin
-  const [pass, setPass] = useState('')
+  const [pass, setPass] = useState()
   const [loginError, setLoginError] = useState()
-  const [loader, setLoader] = useState()
+  const [loginLoader, setLoginLoader] = useState(false)
   const navigate = useNavigate()
 
   const submitLogin =  async e => {
     e.preventDefault()
-    setLoader(true)
+    setLoginLoader(true)
     try {
       await loginEmailPass(userToLogin.email, pass)
       navigate('/')
     } catch (err) {
       setLoginError(`${err.code.replace("auth/","")}`)
-      setLoader()
+      setLoginLoader(false)
     }
   }
-  
+
   return (
-    <main className="grow flex flex-col">
-      {loader
-      ? <div className="grow flex items-center justify-center">
-          <div
-            className="inline-block h-14 w-14 animate-spin rounded-full text-blue-500 border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-            role="status">
-          </div>
-        </div>
-      : <section className="w-full md:max-w-[33rem] mx-auto md:mt-12 px-6 md:px-12 md:py-12 grow md:grow-0 bg-white md:border md:border-slate-300 md:rounded-md">
-          <h2 className="mt-6 md:mt-0 text-2xl font-medium">Ingresá tu contraseña de FRIMarket</h2>
-          <div className="w-max mt-4 px-2 py-2 flex items-center border border-slate-300 rounded-full">
-            <img src={photoURL} alt={idUser} className="w-8 h-8 flex flex-none rounded-full"/>
-            <div className="px-2 flex flex-col grow text-xs">
+    <section className="login">
+      <div className="login-form">
+        <h2>Ingresá tu contraseña de FRIMarket</h2>
+        <div className="login-inputs">
+          <div className="login-user">
+            <img src={photoURL} alt={idUser}/>
+            <div className="flex flex-col grow text-xs">
               <span>{email}</span>
               <Link to='/login' className="w-max text-blue-500">Cambiar cuenta</Link>
             </div>
           </div>
-          <div className="relative mt-6">
+          <div className="login-input">
             <InputPassword 
             label='Contraseña'
-            value={pass}
+            value={pass || ''}
             id='password'
             name='password'
             onChange={({target:{value}}) => setPass(value)}
             required
             />
             {loginError &&
-              <p className="top-full left-2 absolute w-max pt-0.5 flex items-center text-[0.8rem] text-red-500">
-                <i className="fa-solid fa-circle-exclamation"></i>
-                <span className="pl-2 font-medium">{loginError}</span>
-              </p>
-            }
+              <Notification message={loginError}/>}
           </div>
-          <div className="mt-6 flex">
-            <button onClick={submitLogin} className="mr-4 btn btn-m btn-blue">
-              <span className="text-sm font-medium">Iniciar sesion</span>
-            </button>
-          </div>
-        </section>}
-    </main>
+        </div>
+        <div className="login-buttons">
+          {loginLoader
+          ? <ButtonLoader />
+          : <button 
+            onClick={submitLogin} 
+            disabled={!pass}
+            className="btn btn-m btn-blue btn-text"
+            >
+              Iniciar sesion
+            </button>}
+        </div>
+      </div>
+    </section>
   )
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
-import { useLoginContext } from "../../context/LoginContext/LoginContext.jsx"
+import { useLoginContext } from "./context/LoginContext.jsx"
 import { getSnapUser, getSnapUserById } from "../../services/user.js"
-import { InputText } from "../../components/index.js"
+import { ButtonLoader, InputText, Notification } from "../../components/index.js"
 
 export const Login = () => {
   const { setUserToLogin } = useLoginContext()
-  const [userToFind, setUserToFind] = useState('')
-  const [loader, setLoader] = useState()
-  const [userToFindError, setUserToFindError] = useState()
+  const [userToFind, setUserToFind] = useState()
+  const [userToFindLoader, setUserToFindLoader] = useState()
+  const [userToFindError, setUserToFindError] = useState(false)
   const navigate = useNavigate()
 
   const findUser = async e => {
     e.preventDefault()
-    setLoader(true)
+    setUserToFindLoader(true)
     if(userToFind.includes('@')) {
       try {
         const user = await getSnapUser(userToFind)
@@ -24,18 +24,18 @@ export const Login = () => {
           })
           navigate('/login/enter-pass')
         } else {
-          setLoader()
+          setUserToFindLoader(false)
           setUserToFindError('Revisa el e-mail.')
         }
       } catch (err) {
-        setLoader()
+        setUserToFindLoader(false)
         setUserToFindError('Ocurrió un error, inténtalo más tarde.')
       }
     } else {
       try {
         const user = await getSnapUserById(userToFind)
         if(!user) {
-          setLoader()
+          setUserToFindLoader(false)
           setUserToFindError('Revisa el nombre de usuario.')
         } else {
           setUserToLogin({
@@ -45,49 +45,47 @@ export const Login = () => {
           navigate('/login/enter-pass')
         }
       } catch (err) {
-        setLoader()
+        setUserToFindLoader(false)
         setUserToFindError('Ocurrió un error, inténtalo más tarde.')
       }
     }
   }
 
   return (
-    <main className="grow flex flex-col">
-      {loader
-      ? <div className="grow flex items-center justify-center">
-          <div
-            className="inline-block h-14 w-14 animate-spin rounded-full text-blue-500 border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-            role="status">
-          </div>
-        </div>
-      : <section className="w-full md:max-w-[33rem] mx-auto md:mt-12 px-6 md:px-12 md:py-12 grow md:grow-0 bg-white md:border md:border-slate-300 md:rounded-md">
-          <h2 className="mt-6 md:mt-0 text-2xl font-medium">Ingresá tu e‑mail o usuario de FRIMarket</h2>
-          <div className="relative mt-6">
+    <section className="login">
+      <div className="login-form">
+        <h2>Ingresá tu e‑mail o usuario de FRIMarket</h2>
+        <div className="login-inputs">
+          <div className="login-input">
             <InputText 
             size='input-l'
             label='E‑mail o usuario'
             id='user'
             name='user'
-            value={userToFind} 
-            onChange={e => setUserToFind(e.target.value)}
+            value={userToFind || ''} 
+            onChange={({target:{value}}) => setUserToFind(value)}
             required
             />
             {userToFindError &&
-              <p className="top-full left-2 absolute w-max pt-0.5 flex items-center text-[0.8rem] text-red-500">
-                <i className="fa-solid fa-circle-exclamation"></i>
-                <span className="pl-2 font-medium">{userToFindError}</span>
-              </p>
-            }
+              <Notification message={userToFindError}/>}
           </div>
-          <div className="mt-6 flex">
-            <button onClick={findUser} className="mr-4 btn btn-m btn-blue">
-              <span className="text-sm font-medium">Continuar</span>
-            </button>
-            <Link to='/registration' className="btn btn-text-blue btn-m">
-              <span className="text-sm font-medium">Crear cuenta</span>
-            </Link>
-          </div>
-        </section>}
-    </main>
+        </div>
+        <div className="login-buttons">
+          {userToFindLoader
+          ? <ButtonLoader />
+          : <button 
+            onClick={findUser} 
+            className="btn btn-m btn-blue btn-text"
+            disabled={!userToFind}
+            >
+              Continuar
+            </button>}
+          <Link to='/registration' className="btn btn-text-blue btn-m btn-text">
+            Crear cuenta
+          </Link>
+        </div>
+      </div>
+    </section>
   )
+
 }
